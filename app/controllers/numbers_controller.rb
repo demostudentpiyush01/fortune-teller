85dd78b3261ad827ec3044063935d62c0baee478
prpdpigithub <pikoit60@gmail.com>
2023-02-24 17:10:27 +0000

    Commit 1

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,27 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+  def loosers
+    @zebra = Array.new
+
+    5.times do
+      giraffe = rand(1...100)
+      
+      @zebra.push(giraffe)
+    end
+
+    render({ :template => "lottery_stuff/woohoo1.html.erb"})
+  end 
+  def dice_1
+    @zebra = Array.new
+
+    1.times do
+      giraffe = rand(1...6)
+      
+      @zebra.push(giraffe)
+    end
+
+    render({ :template => "dice/dice1.html.erb"})
+  end 
 end
